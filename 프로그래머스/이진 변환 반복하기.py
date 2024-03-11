5eb47d40e23f934dba93cda94d3d63b10fc66e45
harii-in <charin1130@naver.com>
2024-03-11 12:05:10 +0900

    Add: 이진 변환 반복하기.py

---
+++ 프로그래머스/이진 변환 반복하기.py
@@ -0,0 +1,22 @@
+def solution(s):
+    answer = []
+    cnt = 0     
+    zeroCnt = 0
+    
+    #"1"이 남을 때까지 반복
+    while True:
+        if s == "1":
+            break
+        
+        # 문자열의 0의 개수 세기
+        zeroCnt += s.count("0")
+        # 0을 공백으로 변환
+        s = s.replace("0",'')
+        # 2진수로 변환
+        s = bin(len(s))[2:]
+        
+        cnt += 1
+    
+    # 이진 변환의 횟수와 변환 과정에서 제거된 모든 0의 개수
+    answer = [cnt, zeroCnt]    
+    return answer
